db1ac72c39d579f8fbdeb90dbb3d9c5f4600d6c4
Jenny Kim <kimj1905@gmail.com>
2021-09-08 15:20:14 -0400

    commit tree e01cd247edc54ea050b4cb2d13cb79002d488e21

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random())// + 1;
+
+const num3 = 23;
+const num4 = 19;
+const mod = num3 % num4;
+
+const max = Math.max(5, 20)
